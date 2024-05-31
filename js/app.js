92677f027f14b17ba91413a2d41673adaf73ccdd
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-05-31 12:45:29 +0330

    take url of site

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
+
+
 //access to the form values
-document.querySelector('#generate-names').addEventListener('submit',loadNames)
+document.querySelector("#generate-names").addEventListener("submit", loadNames);
 
 //read the values and generate URL
-function loadNames(e){
-    e.preventDefault();
+function loadNames(e) {
+  e.preventDefault();
 
-    //read the values from the form
-    const country=document.querySelector('#country').value,
-      genre=document.querySelector('#genre').value
+  //read the values from the form
+  const country = document.querySelector("#country").value,
+    genre = document.querySelector("#genre").value;
 
-    console.log(country);  
-    console.log(genre);  
+  // generate the URL
+  let url = "https://randomuser.me/";
+  if(country!==''){
+      url += "api/?nat=" + country+'/';
+  }else{
+    url += "api/?nat=gb"; //default to UK
+  }
+  if(genre !==''){
+    url += (genre+'/')
+  }else{
+    url+='random/'
+  }
+
+  console.log(url);
+  console.log(genre);
 }
